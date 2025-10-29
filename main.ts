94fc2d087b35dc3e170faa0d1e06ed893967e174
Le438 <leabeekhuis@web.de>
2025-10-29 14:25:51 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
-
+basic.forever(function () {
+    if (led.brightness() < 79) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.setLedColor(0x999999)
+    } else if (input.lightLevel() > 80 && input.lightLevel() < 180) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        basic.setLedColor(0x00ff00)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.setLedColor(0xff0080)
+    }
+})
